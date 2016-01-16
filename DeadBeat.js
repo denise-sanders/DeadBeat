118f1c0289abcfc88de0223bbd4db39d0ef8a105
dairvin9 <dairvin9@tamu.edu>
2016-01-16 12:34:12 -0600

    Renamed StartState

--- DeadBeat.js
+++ DeadBeat.js
@@ -18,34 +18,36 @@ GameState.prototype.enable = function() {};     // mark this state as active
 GameState.prototype.tick = function() {};       // tick the state
 GameState.prototype.disable = function() {};    // mark this state as inactive
 
-// PlayState extends GameState
+// StartState extends GameState
 
-var PlayState = function(phGame) {
-    this.name = "PLAYSTATE";
+var StartState = function(phGame) {
+    this.name = "StartState";
     this.game = phGame;
 
     this.game.load.image('title', 'assets/DeadBeatTitle.png');
     this.game.load.image('note', 'assets/note.png');
+	this.game.load.image('start', 'assets/StartButton.png');
+	
     this.game.load.audio('song', 'assets/song.ogg');
 
     this.backgroundColorValue = 0xff0000;
     this.incrementer = 1;
 }
 
-PlayState.prototype = Object.create(GameState.prototype);
+StartState.prototype = Object.create(GameState.prototype);
 
-PlayState.prototype.enable = function() {
+StartState.prototype.enable = function() {
     this.game.stage.backgroundColor = this.backgroundColorValue;
     this.game.add.sprite(25, 25, 'title');
 
     this.note = this.game.add.sprite(-100, -100, 'note');
     this.note.scale.setTo(.2,.2);
 
-    this.startButton = this.game.add.button(game.world.centerX - 95, 400, 'Start',
+    this.startButton = this.game.add.button(game.world.centerX - 95, 400, 'start',
             this.actionOnClick, this, 2, 1, 0);
 
     this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    // Prevents the browswer from taking the signal
+    // Prevents the browser from taking the signal
     this.game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
 
     var textSpaceHeight = 300;
@@ -54,7 +56,7 @@ PlayState.prototype.enable = function() {
         font: "100px Arial", fill: "#ffffff", align: "center" });
 }
 
-PlayState.prototype.tick = function() {
+StartState.prototype.tick = function() {
     this.updateBackgroundColor();
 
     // Using downDuration lets us display it longer
@@ -70,19 +72,20 @@ PlayState.prototype.tick = function() {
 
 // Utilities for input and colors.
 
-PlayState.prototype.actionOnClick = function() {
-    console.log("Clicked!");
+StartState.prototype.actionOnClick = function() {
+    this.startButton.visible = false;
+	// Call next state
 }
 
-PlayState.prototype.showNote = function() {
+StartState.prototype.showNote = function() {
     this.note.reset(100,200);
 }
 
-PlayState.prototype.hideNote = function() {
+StartState.prototype.hideNote = function() {
     this.note.reset(-100,-100);
 }
 
-PlayState.prototype.updateBackgroundColor = function() {
+StartState.prototype.updateBackgroundColor = function() {
     switch (this.backgroundColorValue) {
         case 0xff0000:              // red
             this.incrementer = 0x100;    // approaches yellow
@@ -116,24 +119,24 @@ var game = new Phaser.Game(width, height, Phaser.AUTO, '', {
     preload: preload, create: create, update: update
 });
 
-var playState;
+var StartState;
 
 var currentState;
 
 function preload() {
-    playState = new PlayState(game);
+    StartState = new StartState(game);
 }
 
 function create() {
-    currentState = playState;
+    currentState = StartState;
     currentState.enable();
 }
 
 function update() {
     var oldState = currentState;
     switch (currentState.tick()) {
-        case "PLAYSTATE":
-            currentState = playState;
+        case "StartState":
+            currentState = StartState;
             break;
     }
     if (oldState != currentState) {
