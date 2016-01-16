0aab6b8114c9479668bb29e75795adc444e079de
dairvin9 <dairvin9@tamu.edu>
2016-01-16 00:01:48 -0600

    Adding title

--- DeadBeat.js
+++ DeadBeat.js
@@ -1,11 +1,20 @@
-var width = document.body.offsetWidth;
+var width = document.body.offsetWidth; 
 var height = document.body.offsetHeight;
+
 var game = new Phaser.Game(width, height, Phaser.AUTO, '', {
     preload: preload, create: create, update: update });
 
-function preload() {
+function preload() 
+{
+	game.load.image('title', 'DeadBeatTitle.png');
+	// alert("Ellen loves us")
 }
-function create() {
+
+function create() 
+{
+	game.add.sprite(0, 0, 'title');
 }
-function update() {
+function update() 
+{
+
 }
