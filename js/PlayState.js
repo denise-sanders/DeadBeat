f994a5a79c8d7b2322ae95636c84755391f14e95
Randall <takmo@bitwisehero.com>
2016-01-16 17:19:25 -0600

    Input and beat matching is a bit more fine-tuned.

--- js/PlayState.js
+++ js/PlayState.js
@@ -7,11 +7,6 @@ var PlayState = function(phGame) {
     // Things to load go here.
     this.game.load.image('image/note', 'assets/note.png');
     this.game.load.audio('song/oceanic', 'assets/oceanic.ogg');
-    
-    // Create dicts for these to go in.
-    this.inputs = {}
-    this.songs = {}
-    this.sprites = {}
 };
 
 PlayState.prototype = Object.create(GameState.prototype);
@@ -23,41 +18,73 @@ PlayState.prototype.enable = function() {
     // Setup note in the middle of the screen.
     var centerX = game.world.centerX;
     var centerY = game.world.centerY;
-    this.sprites.note = this.game.add.sprite(centerX, centerY, 'image/note');
-    this.sprites.note.renderable = false;
+    this.note = this.game.add.sprite(centerX, centerY, 'image/note');
+    this.note.renderable = false;
 
     // Setup the spacebar to count as a clap.
-    this.inputs.space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    this.space = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     this.game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]); // browser can't stop the signal, Mel
 
     // Prepare the song.
-    this.songs.oceanic = this.game.add.audio('song/oceanic');
+    this.song = this.game.add.audio('song/oceanic');
     this.bpm = 90;
+
+    // Prepare the beat information.
+    this.beatTime = 60 / this.bpm;
+    this.beats = 0;
+    this.threshold = this.beatTime * 0.2;
+
+    // Prepare SpaceBar tracking information.
+    this.lastInput = 0;
+    this.inputFreq = this.beatTime / 2;
+
+    // Note display information.
+    this.displayNoteUntil = this.currentTime - 1;
 
     // Keep track of time.
     this.startTime = this.game.time.totalElapsedSeconds();
+    this.lastTime = this.startTime;
+
+    // Factor in a minor offset for audio.
+    this.lastTime -= 0.2;
 
     // OK GO!
-    this.songs.oceanic.play();
+    this.song.play();
 }
 
 PlayState.prototype.tick = function() {
-    // Was the spacebar pressed?
-    if (this.inputs.space.downDuration(500)) {
-        // Hows close was this to the beat?
-        elapsedTime = this.game.time.totalElapsedSeconds() - this.startTime;
-        beatTime = 60 / this.bpm;
-        elapsedBeats = Math.floor(elapsedTime / beatTime);
-        remainder = elapsedTime - (elapsedBeats * beatTime);
-        
-        // Was it close enough?
-        if (Math.abs(beatTime - remainder) < 0.2) {
-            this.sprites.note.renderable = true;
-        } else {
-            console.log("not close nough, bruh: " + elapsedBeats);
-        }
-    } else {
-        this.sprites.note.renderable = false;
+    // Update the time/beat information.
+    var currentTime = this.game.time.totalElapsedSeconds();
+    while (currentTime > (this.lastTime + this.beatTime)) {
+        this.beats += 1;
+        this.lastTime += this.beatTime;
     }
-    return this.name;
+    
+    // Get surrounding beat information.
+    var timeSinceLast = currentTime - this.lastTime;
+    var timeUntilNext = this.lastTime + this.beatTime - currentTime;
+
+    // Did the user press the SpaceBar? Were they allowed to?
+    if (this.space.isDown && (currentTime - this.lastInput) > this.inputFreq) {
+        this.lastInput = currentTime;
+        // Does the beat count?
+        if (timeSinceLast < this.threshold) {
+            this.note.renderable = true;
+            this.displayNoteUntil = this.lastTime + this.threshold * 2;
+            console.log("Off by: " + timeSinceLast);
+        } else if (timeUntilNext < this.threshold) {
+            this.note.renderable = true;
+            this.displayNoteUntil = this.lastTime + this.beatTime + this.threshold * 2;
+            console.log("Off by: -" + timeUntilNext);
+        }
+    }
+
+    // Verify that the note is in it's correct visibility state.
+    if (currentTime > this.displayNoteUntil) {
+        this.note.renderable = false;
+    }
 }
+
+// RULES
+// (1) User can only press the spacebar once every beatTime*(2/3) seconds.
+// (2) Must be within 20% of the beat in order to count.
