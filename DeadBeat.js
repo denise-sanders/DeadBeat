6c28551c680916b315d884071b00cb6848467dc9
dairvin9 <dairvin9@tamu.edu>
2016-01-16 00:32:04 -0600

    objects now visible

--- DeadBeat.js
+++ DeadBeat.js
@@ -1,18 +1,29 @@
 var width = document.body.offsetWidth; 
 var height = document.body.offsetHeight;
 
-var game = new Phaser.Game(width, height, Phaser.AUTO, '', {
-    preload: preload, create: create, update: update });
+//var game = new Phaser.Game(width, height, Phaser.AUTO, '', {
+//    preload: preload, create: create, update: update });
+
+var game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() 
 {
-	game.load.image('title', 'DeadBeatTitle.png');
-	// alert("Ellen loves us")
+	game.load.image('title', 'assets/DeadBeatTitle.png');
+	//game.load.image('cat', 'assets/uglyCat.png');
+
 }
+
+var platforms;
 
 function create() 
 {
-	game.add.sprite(0, 0, 'title');
+	//game.add.sprite(0, 0, 'title');
+	
+	
+   
+    game.add.sprite(25, 25, 'title');
+	//game.add.sprite(0, 0, 'cat');
+
 }
 function update() 
 {
