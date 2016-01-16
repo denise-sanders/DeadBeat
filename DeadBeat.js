7f4bdd1d7b60220b160ee518c47fae30569626d6
Randall <takmo@bitwisehero.com>
2016-01-15 23:04:32 -0600

    Added basic stuff, ya know.

---
+++ DeadBeat.js
@@ -0,0 +1,11 @@
+var width = document.body.offsetWidth;
+var height = document.body.offsetHeight;
+var game = new Phaser.Game(width, height, Phaser.AUTO, '', {
+    preload: preload, create: create, update: update });
+
+function preload() {
+}
+function create() {
+}
+function update() {
+}
